6207ab067f0398aae09a7ee2b48acf15ac9d328c
virajsaranga <virajridi@gmail.com>
2024-10-19 12:33:39 +0530

    slice file created

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,66 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
+
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      // The item to add to the cart
+      // NOTE: we don't need user, rating, numReviews or reviews
+      // in the cart
+      const { user, rating, numReviews, reviews, ...item } = action.payload;
+
+      // Check if the item is already in the cart
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        // If exists, update quantity
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        // If not exists, add new item to cartItems
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      // Update the prices and save to storage
+      return updateCart(state, item);
+    },
+    removeFromCart: (state, action) => {
+      // Filter out the item to remove from the cart
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+
+      // Update the prices and save to storage
+      return updateCart(state);
+    },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+    savePaymentMethod: (state, action) => {
+      state.paymentMethod = action.payload;
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+    clearCartItems: (state, action) => {
+      state.cartItems = [];
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+    resetCart: (state) => (state = initialState),
+  },
+});
+
+export const {
+  addToCart,
+  removeFromCart,
+  saveShippingAddress,
+  savePaymentMethod,
+  clearCartItems,
+  resetCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
